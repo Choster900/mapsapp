f293e100eea7d7494836bf1597aba7b3ba83fb55
Choster900 <16adonaysergio@gmail.com>
2025-07-17 10:51:08 -0600

    refactor: simplify Map component by removing unused props and code

--- src/presentation/components/Map.tsx
+++ src/presentation/components/Map.tsx
@@ -1,60 +1,40 @@
-import React, { useMemo } from 'react';
+import React from 'react'
 import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { Location } from '../../infraestructure/interfaces/location';
 
 interface MapProps {
     showUserLocation?: boolean;
-    initialLocation: Location;
-    onRegionChange?: (region: any) => void;
-    zoomLevel?: {
-        latitudeDelta: number;
-        longitudeDelta: number;
-    };
+    initialLocation: Location
 }
 
-const DEFAULT_ZOOM = {
-    latitudeDelta: 0.015,
-    longitudeDelta: 0.0121,
-};
-
-const Map: React.FC<MapProps> = ({
-    showUserLocation = true,
-    initialLocation,
-    onRegionChange,
-    zoomLevel = DEFAULT_ZOOM,
-}) => {
-    // Memorizar la región inicial para evitar recálculos
-    const initialRegion = useMemo(
-        () => ({
-            latitude: initialLocation.latitude,
-            longitude: initialLocation.longitude,
-            ...zoomLevel,
-        }),
-        [initialLocation.latitude, initialLocation.longitude, zoomLevel]
-    );
-
+export default function Map({ showUserLocation = true, initialLocation }: MapProps) {
     return (
-        <MapView
-            showsUserLocation={showUserLocation}
-            provider={PROVIDER_GOOGLE}
-            style={{ flex: 1 }}
-            initialRegion={initialRegion}
-            followsUserLocation={true}
-            onRegionChangeComplete={onRegionChange}
-        >
-            {/* Marcador comentado - descomenta si necesitas usarlo
-      <Marker
-        coordinate={{
-          latitude: initialLocation.latitude,
-          longitude: initialLocation.longitude,
-        }}
-        title={"Marker Title"}
-        description={"Marker Description"}
-        image={require('../../assets/custom-marker.png')}
-      />
-      */}
-        </MapView>
-    );
-};
+        <>
+            <MapView
+                showsUserLocation={showUserLocation}
+                provider={PROVIDER_GOOGLE}
+                style={{ flex: 1 }}
+                initialRegion={{
+                    latitude: initialLocation.latitude,
+                    longitude: initialLocation.longitude,
+                    latitudeDelta: 0.015,
+                    longitudeDelta: 0.0121,
+                }}
+                followsUserLocation={true} // << esto sigue el movimiento del usuario
 
-export default Map;
+            >
+                {/*  <Marker
+                    coordinate={{
+                        latitude: initialLocation.latitude,
+                        longitude: initialLocation.longitude,
+                    }}
+                    title={"Marker Title"}
+                    description={"Marker Description"}
+
+                    image={require('../../assets/custom-marker.png')}
+                /> */}
+
+            </MapView>
+        </>
+    )
+}
